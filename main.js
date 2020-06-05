a0a11f518ed801e479f1a9b47b72f43126f8df6e
huseyin <huseyindasci.tr@gmail.com>
2020-06-05 22:04:53 +0300

    woy deneme

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $("#s11").click(function () { 
+    $("#firstslide").attr("src","images/slider-main-demo-1.jpg" )    
+    
+  });
+  $("#s12").click(function () { 
+    $("#firstslide").attr("src","images/slider-main-demo-1-2.jpg" )    
+    
+  });
+  $("#s13").click(function () { 
+    $("#firstslide").attr("src","images/slider-main-demo-1-3.jpg" )    
+    
+  });
+  $("#s14").click(function () { 
+    $("#firstslide").attr("src","images/slider-main-demo-1-4.jpg" )    
+    
+  });
+  //second slide
+  $("#s2").click(function () { 
+    $("#secondslide").attr("src","images/slider-main-demo-2.jpg" )    
+    
+  });
+  $("#s21").click(function () { 
+    $("#secondslide").attr("src","images/slider-main-demo-2-1.jpg" )    
+    
+  });
+  $("#s22").click(function () { 
+    $("#secondslide").attr("src","images/slider-main-demo-2-2.jpg" )    
+    
+  });
+
+
+
+});
